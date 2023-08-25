6775d6f8b75cf61f61f94c7ddd9e4ab9f6dc4998
moustaxx <maciasm1303@gmail.com>
2023-08-25 20:02:09 +0200

    Fix eslint no unused var warning

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import type notifier from 'node-notifier';
+import type {} from 'node-notifier';
 
 declare module 'node-notifier' {
     interface NotificationMetadata {
